3dec1a3c104bb365291bfe3c4cf15bb2a5a88025
anijahp <anijah.phillip@students.cookman.edu>
2023-05-15 16:32:21 -0400

    formatted the results table.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from openpyxl import load_workbook
 import os
+from openpyxl.styles import Font
 
 app = Flask(__name__)
 
@@ -11,6 +12,20 @@ def display_workbook():
 
     # Extract the sheet names
     sheet_names = workbook.sheetnames
+
+    # Apply formatting to the pivot table in each sheet
+    for sheet_name in sheet_names:
+        sheet = workbook[sheet_name]
+        max_row = sheet.max_row
+
+        # Apply bold font to the left column (A column)
+        for row in sheet.iter_rows(min_row=1, max_row=max_row, min_col=1, max_col=1):
+            for cell in row:
+                cell.font = Font(bold=True)
+
+        # Apply larger font size to the title (A1 cell)
+        title_cell = sheet['A1']
+        title_cell.font = Font(size=14)
 
     base_dir = os.path.dirname(os.path.abspath(__file__))
 
